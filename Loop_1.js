3a4cdbf04941753bafce03349fade27597aa3e1d
Zuri Barnes <45982373+zuriBarnes@users.noreply.github.com>
2021-07-12 17:21:11 -0400

    Create Loop_1.js
    initial szangha commit

---
+++ Loop_1.js
@@ -0,0 +1,23 @@
+"use strict";
+
+//		javascript code
+//		script_name:
+//
+//		author: 
+//		description:
+//
+
+init();
+setTempo(125);
+fitMedia(TECHNO_MAINLOOP_019, 1, 1, 2.97)  
+fitMedia(TECHNO_MAINLOOP_019, 1, 3, 17)
+fitMedia(RD_TRAP_DARKPAD_1, 2, 1, 1.5)
+fitMedia(RD_TRAP_DARKPAD_1, 2, 3, 3.5)
+fitMedia(RD_TRAP_DARKPAD_1, 2, 5, 5.5)
+fitMedia(RD_TRAP_DARKPAD_1, 2, 7, 7.5)
+fitMedia(RD_TRAP_DARKPAD_1, 2, 9, 9.5)
+fitMedia(RD_TRAP_DARKPAD_1, 2, 11, 11.5)
+fitMedia(RD_TRAP_DARKPAD_1, 2, 13, 13.5)
+fitMedia(RD_TRAP_BASSDROPS_1, 3, 7, 7.5)
+fitMedia(RD_ELECTRO_SFX_AIRPULSE_3, 4, 7, 7.5)
+finish();
